e47052163120e94e136ae675384394d64d99ff65
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2023-07-22 18:05:17 +0200

    [login page, Edge translation awareness] improved html (true HTML document, not a minimal fragment), improved translations by Bing in MS Edge (translations were breaking by changing "value" of "submit"), improved elements layout by re-arranging, improved navigation by link

--- login.php
+++ login.php
@@ -1,22 +1,29 @@
 <?php session_start();
 include "secrets.php";
 if(!isset($secret_password)) die("error: missing a secrets.php file!");
-if($_REQUEST["submit"]=="login" && $_REQUEST["password"]==$secret_password){
+if(isset($_REQUEST["submit-enter"]) && $_REQUEST["password"]==$secret_password){
 $_SESSION["loggedin"]=true;
 }
-if($_REQUEST["submit"]=="logout"){
+if(isset($_REQUEST["submit-exit"])){
 $_SESSION["loggedin"]=false;
 }
-?><!doctype html>
-<title>login</title>
+?><!doctype html> <html lang="en"> <head>
+
+<title>enter</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="utf-8">
-Status: <?=$_SESSION["loggedin"]?"logged in":"logged out"?>
+
+</head><body>
+
+<a href=".">go to main page</a>
+<br>
 <form action="" method="post">
-
+Condition: <?=$_SESSION["loggedin"]?"access enabled":"access not enabled"?>
+<input type="submit" value="exit" name="submit-exit"><br>
+<hr>
 <input type="email" name="email" placeholder="e-mail address">
-<input type="password" name="password" placeholder="pass-word"> <br>
-
-<input type="submit" value="login" name="submit">
-<input type="submit" value="logout" name="submit">
+<input type="password" name="password" placeholder="pass-word">
+<input type="submit" value="enter" name="submit-enter">
 </form>
+
+</body></html>
