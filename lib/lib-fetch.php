1cc6df45aaea8ebadfd095a347da62665184c315
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2023-07-23 09:34:08 +0200

    ["library" (means re-usable)] title from URL. lib/lib-fetch.php: to get HTML "title from URL" of HTML, falling-back to URL if URL has no "title".

---
+++ lib/lib-fetch.php
@@ -0,0 +1,28 @@
+<?php
+
+function title_from_URL($url){
+
+    function file_get_contents_curl($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
+    $html = file_get_contents_curl($url);
+
+    function title_from_HTML($html, $no_title_fallback){
+        preg_match('/<title>(.*)<\/title>/',$html,$matches);
+        if(!isset($matches[1])) $title = $no_title_fallback; else
+        $title = $matches[1];
+        return $title;
+    }
+
+    $title = title_from_HTML($html, $url);
+
+    return $title;
+}
